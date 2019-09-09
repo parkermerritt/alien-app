82163fe9691629fa8c9a40ae2f02fa82fee2e14c
parkermerritt <parker.merritt@gmail.com>
2019-09-09 18:52:21 -0400

    Add alien words

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
 var words = {
     "apple": {
         translation: "Pomme",
-        meaning: "fruit, red, sweet",
-        use: "For eating",
-        etymology: "From middle english Appel"
     },
     "bowling": {
         translation: "Blah blah",
-        meaning: "sport",
-        use: "For recreation",
-        etymology: "Some old sport"
-    }
+    },
+    /* Note 1 */
+    "ouuaieaiie": {
+        translation: "One day on Venus is longer than one WHOLE year on Earth!",
+    },
+    "eeouiaaaeu": {
+        translation: "The largest known asteroid is 600 miles wide.",
+    },
+    "ieieooooauu": {
+        translation: "The moon was once part of Earth!",
+    },
+    "aieoouiua": {
+        translation: "There is a volcano on Mars 3 times bigger than Mt. Everest.",
+    },
+    "iiieaoueeo": {
+        translation: "The moon was once part of Earth!",
+    },
+    "ooiaueiuei": {
+        translation: "The moon was once part of Earth!",
+    },
+    "aeiiuuoe": {
+        translation: "The moon was once part of Earth!",
+    },
+    "oiiueaie": {
+        translation: "The moon was once part of Earth!",
+    },
+    "eieioouaie": {
+        translation: "The moon was once part of Earth!",
+    },
+    "eeioouuie": {
+        translation: "The moon was once part of Earth!",
+    },
+    /* Note 2 */
+    "eiiouuaau": {
+        translation: "The moon was once part of Earth!",
+    },
+    "oooiueeai": {
+        translation: "The moon was once part of Earth!",
+    },
+    "uuiiaeiai": {
+        translation: "The moon was once part of Earth!",
+    },
+    "aiaeeuioo": {
+        translation: "The moon was once part of Earth!",
+    },
+    "eeeooiuai": {
+        translation: "The moon was once part of Earth!",
+    },
+    "uuioioae": {
+        translation: "The moon was once part of Earth!",
+    },
+    "uooiaeeaoiu": {
+        translation: "The moon was once part of Earth!",
+    },
+    "uoiieaui": {
+        translation: "The moon was once part of Earth!",
+    },
+    "iiioueeuiei": {
+        translation: "The moon was once part of Earth!",
+    },
+    "eiouuai": {
+        translation: "The moon was once part of Earth!",
+    },
 };
 
 window.onload = onloadFunc;
